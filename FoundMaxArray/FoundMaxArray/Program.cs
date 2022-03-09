c9bd62a7d467086cdec972d625d49cce5eba1467
RashidMmdov <tu7lw2f82@code.edu.az>
2022-03-10 00:22:33 +0400

    Add MethodPracticeTaskkkS

---
+++ FoundMaxArray/FoundMaxArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FoundMaxArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Arr = { 2, 5, 6, 7, 11, 8, 9 };
+            Console.WriteLine(findMax(Arr));
+        }
+        public static int findMax(int[] Arr)
+        {
+            int MaxNum = Arr[0];
+            foreach (int numbers in Arr)
+            {
+                if (numbers > MaxNum)
+                {
+                    MaxNum = numbers;
+                }
+            }
+            return MaxNum;
+        }
+    }
+}
---
+++ newTask 3(123)/newTask 3(123)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace newTask_3_123_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(addedProsses(number));
+        }
+        public static  int addedProsses(int num)
+        {
+            int tempNumber = num;
+            int tempValue = 3;
+            while (num > 0)
+            {
+                int rem = num % 10;
+                tempValue *= 10;
+                num /= 10;
+
+            }
+            return tempNumber + tempValue;
+        }
+    }
+}
